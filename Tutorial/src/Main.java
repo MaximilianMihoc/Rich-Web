7255e32338ea23f97d96f2a073664ac8ee924dfd
MaximilianMihoc <maximilian.mihoc@mydit.ie>
2015-09-24 14:02:44 +0100

    commit message

--- Tutorial/src/Main.java
+++ Tutorial/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome back Git, after ssh key added");
+		System.out.println("Welcome back Git");
 	}
 
 }
